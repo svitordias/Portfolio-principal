5ec5cc76131aef25499f698005c2dd4b542d705d
Vitor Dias <vitordias-sousa@hotmail.com>
2023-05-09 15:45:33 -0300

    Escrita digitada (inicio-h2)

---
+++ scripts/escritor.js
@@ -0,0 +1,18 @@
+const escritor = document.getElementById("escritor");
+let line = [
+    "Vitor Dias Sousa", "Seja bem vindo!"
+];
+let i = 0, j = 0, wait =6;
+setInterval(() => {
+    if (wait-- > 0) return;
+    if (wait == -1) escritor.innerHTML = "";
+    if (i < line.length)
+        if (j < line[i].length)
+            escritor.innerHTML += line[i][j++];
+        else
+            wait = 8, j = 0, i++;
+        else
+            wait = 8, i = j = 0;
+}, 200)
+
+escritor()
